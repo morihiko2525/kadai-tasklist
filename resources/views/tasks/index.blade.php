0400c8700be50072903a85a7c17dec914dce66fe
Yuki Mori <morihiko223@gmail.com>
2021-04-23 00:53:04 +0000

    ログイン中にSign upが表示されていたものを削除

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -13,8 +13,6 @@
         <h1>タスク一覧</h1>
         {!! link_to_route('tasks.create', '新規タスクの追加', [], ['class' => 'btn btn-primary']) !!} 
         
-        {!! link_to_route('signup.get', 'Sign up now!', [], ['class' => 'btn btn-lg btn-primary']) !!}
-        
          @if (count($tasks) > 0)
             <table class="table table-striped">
                 <thead>
